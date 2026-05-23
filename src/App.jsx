0de099be16c7e5943ec78351e2ce320044deb172
D2Z11 <133183204+D2Z11@users.noreply.github.com>
2026-05-23 14:43:53 +0000

    Finished The Lab

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,40 @@ function App() {
         Earn More, Pay Less? Average Housing Costs and Incomes by NYC Borough
       </h1>
 
-      <DataView />
+      <DataView
+        borough="Staten Island"
+        renterCost="$14,292"
+        ownerCost="$28,752"
+        renterIncome="$37,882"
+        ownerIncome="$94,177" />
+
+      <DataView
+        borough="Queens"
+        renterCost="$16,812"
+        ownerCost="$29,256"
+        renterIncome="$46,549"
+        ownerIncome="$80,144" />
+
+      <DataView
+        borough="Brooklyn"
+        renterCost="$15,144"
+        ownerCost="$31,908"
+        renterIncome="$40,128"
+        ownerIncome="$83,737" />
+
+      <DataView
+        borough="Manhattan"
+        renterCost="$36,252"
+        ownerCost="$18,900"
+        renterIncome="$62,173"
+        ownerIncome="$142,046" />
+
+      <DataView
+        borough="Bronx"
+        renterCost="$13,176"
+        ownerCost="$28,548"
+        renterIncome="$29,302"
+        ownerIncome="$76,454" />
     </div>
   );
 }
